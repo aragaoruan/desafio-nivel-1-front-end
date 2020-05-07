6161790c06ca3244b8b8c10d768cdcf2951bfd6c
Ruan Aragao <aragao.ruan@gmail.com>
2020-05-06 21:48:30 -0300

    feat: buscando repostories

--- src/App.js
+++ src/App.js
@@ -1,8 +1,43 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import "./styles.css";
+
+import api from './services/api';
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    async function getRepositories() {
+      const response = await api.get('repositories');
+
+      setRepositories(response.data)
+    }
+
+    getRepositories()
+  },[]);
+
   return (
-    <h1>Hello world</h1>
+    <div>
+    <ul data-testid="repository-list">
+      
+        {repositories.map(repository => (
+          <div>
+            <li key={repository.id}>
+            {repository.title}
+              <ul>
+                {repository.techs.map(tech => <li key={tech}>{tech}</li>)}
+              </ul>
+              <button onClick={() => {}}>
+                Remover
+              </button>
+            </li>
+        </div> 
+        ))}
+      
+    </ul>
+
+    <button onClick={() => {}}>Adicionar</button>
+  </div>
   );
 }
 
